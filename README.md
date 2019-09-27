fa229991c83391286c93113e78961166ffa189bf
Victor GRENU <victor.grenu@gmail.com>
2019-09-27 14:30:55 +0200

    add support for RDS instances, add LICENSE.

--- README.md
+++ README.md
@@ -7,8 +7,10 @@ I'm using this for non-prod, lab, and personal AWS accounts, to get a kindly rem
 
 ## Core features
 
-* List running EC2 instances across all AWS Regions
-* Check `name`, `instance-id`, `instance_type`, `key_name`, `region`, `launch_time`
+* List running EC2 instances across all AWS Regions.
+  * Check `name`, `instance-id`, `instance_type`, `key_name`, `region`, `launch_time`
+* List running RDS instances across all AWS Regions.
+  * Check `db_instance_name`, `db_engine`, `db_type`, `db_storage`, `region`, `launch_time`
 * Filter / White list capability using a tag
 * Send summary by email once a day
 * Serverless Architecture using Lambda, Lambda layer, SES
@@ -20,22 +22,25 @@ I'm using this for non-prod, lab, and personal AWS accounts, to get a kindly rem
 
 ## Deployment
 
-change emails settings in `handlers.py`
+Change emails settings in `handlers.py`
 
-```
+```bash
 # Email Settings
 recipients = ['ops_team@company.com', 'you@company.com']
 subject = '<your email subject>'
 sender = 'Instance Watcher <ops@company.com>'
 charset = "UTF-8"
 ```
+
+> You will need to validate email received from AWS SES.
+
         $ make layer
         $ make package project=<your_project>
         $ make deploy project=<your_project>
 
 ## Whitelisting
 
-If you want to whitelist a particular EC2 instance to by hidden from the daily report, you will need add the following tag to the instance.
+If you want to whitelist a specific EC2 instance to be hidden from the daily report, you will need add the following tag to the EC2 instance.
 
 | Key | Value |
 |:---:|:-----:|
@@ -45,7 +50,5 @@ If you want to whitelist a particular EC2 instance to by hidden from the daily r
 
 * Create input file for settings or param store (recipients, sender, etc..)
 * Add RDS Instances table
-* Add Detached EBS table
-* Add Detached EIP table
 * Multi-Account Support
-* Add instance role
+* Add instance role column
--- handlers.py
+++ handlers.py
@@ -16,16 +16,42 @@ charset = "UTF-8"
 def main(event, context):
     # Activate mail notifications
     mail_enabled = 1
-    running = []
+    running_ec2 = []
+    running_rds = []
     hidden_count = 0
+    #hidden_rds_count = 0
     account = sts.get_caller_identity().get('Account')
     alias = boto3.client('iam').list_account_aliases()['AccountAliases'][0]
     ec2_regions = [region['RegionName'] for region in ec2.describe_regions()['Regions']]
     # For all AWS Regions
     for region in ec2_regions:
-        conn = boto3.resource('ec2', region_name=region)
         print("Checking running instances in: " + region)
-        instances = conn.instances.filter()
+        
+        # RDS Checking
+        rdscon = boto3.client('rds', region_name=region)
+        rds = rdscon.describe_db_instances()
+        for r in rds['DBInstances']:
+            db_instance_name = r['DBInstanceIdentifier']
+            db_engine =  r['Engine']
+            db_type = r['DBInstanceClass']
+            db_storage = r['AllocatedStorage']
+            db_creation_time = r['InstanceCreateTime'].strftime("%Y-%m-%d %H:%M:%S")
+            db_publicly_accessible = r['PubliclyAccessible']
+
+            running_rds.append({
+                "db_instance_name": r['DBInstanceIdentifier'],
+                "db_engine": r['Engine'],
+                "db_type": r['DBInstanceClass'],
+                "db_storage": r['AllocatedStorage'],
+                "db_publicly_accessible": r['PubliclyAccessible'],
+                "region": region,
+                "launch_time": r['InstanceCreateTime'].strftime("%Y-%m-%d %H:%M:%S")
+            })
+            print(db_instance_name,db_engine,db_type,db_storage,db_creation_time,db_publicly_accessible)
+        
+        # EC2 Checking
+        ec2con = boto3.resource('ec2', region_name=region)
+        instances = ec2con.instances.filter()
         # For every instances in region
         for instance in instances:
             if instance.state["Name"] == "running":
@@ -41,7 +67,7 @@ def main(event, context):
                 if hidden != 1:
                     print(instancename, instance.id)
                     # fill the list
-                    running.append({
+                    running_ec2.append({
                         "instance_name": instancename,
                         "id": instance.id,
                         "instance_type": instance.instance_type,
@@ -51,43 +77,77 @@ def main(event, context):
                     })
             else:
                 print("- No running instance, but some exist (Pending, Stopped or Terminated)")
-    print("Total number of running instance(s):", len(running))
-    print("Total number of hidden instance(s):", hidden_count)
+    print("Total number of running EC2 instance(s):", len(running_ec2))
+    print("Total number of hidden EC2 instance(s):", hidden_count)
+    print("Total number of running RDS instance(s):", len(running_rds))
+    #print("Total number of hidden RDS instance(s):", len(hidden_rds_count)) # not yet implemented
 
 # do a function
-    if len(running) == 0:
-        print("Nothing to do here, no running EC2")
+    if (len(running_ec2) == 0 and len(running_rds) == 0):
+        print("Nothing to do here, no running EC2 or RDS instances")
     else:
         if mail_enabled == 1:
             print("Sending email to: " + str(recipients))
-            body_text = ("Instance Watcher\r\n"
-                        "Running EC2 Instances" + str(len(running))
-                        )
-            body_html = """<html>
-            <head>
-            <style>
-                table, th, td {
-                border: 3px solid black;
-                border-collapse: collapse;
-                }
-            </style>
-            </head>
-            <body>
-            <h1>Instance Watcher 👀</h1>
-            <p>AWS AccountID: <a href="https://""" + account + """.signin.aws.amazon.com/console">""" + account + """</a> - <a href=https://""" + alias + """.signin.aws.amazon.com/console>""" + alias + """</a></p>
-            <h3>Running EC2 Instances: </h3>
-            <table cellpadding="4" cellspacing="4">
-            <tr><td><strong>Name</strong></td><td><strong>Instance ID</strong></td><td><strong>Intsance Type</strong></td><td><strong>Key Name</strong></td><td><strong>Region</strong></td><td><strong>Launch Time</strong></td></tr>
-            """ + \
-                "\n".join([f"<tr><td>{r['instance_name']}</td><td>{r['id']}</td><td>{r['instance_type']}</td><td>{r['key_pair']}</td><td>{r['region']}</td><td>{r['launch_time']}</td></tr>" for r in running]) \
-                + """
-            </table>
-            <p>Total number of running instance(s): """ + str(len(running)) + """
-            <br />Total number of hidden instance(s): """ + str(hidden_count) + """</p>
-            <p><a href="https://github.com/z0ph/instance-watcher">Instance Watcher 🖤</a></p>
-            </body>
+            body_text = (
+                        """
+                        Instance Watcher\r\n
+                        Running EC2 Instances {ec2}
+                        Running RDS Instances {rds}
+                        """).format(
+                                ec2=len(running_ec2),
+                                rds=len(running_rds),
+                            )
+            header = """
+            <html>
+                <head>
+                    <style>
+                        table, th, td {
+                        border: 3px solid black;
+                        border-collapse: collapse;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <h1>Instance Watcher 👀</h1>
+                    <p>AWS AccountID: <a href="https://""" + account + """.signin.aws.amazon.com/console">""" + account + """</a> - <a href=https://""" + alias + """.signin.aws.amazon.com/console>""" + alias + """</a></p>"""
+            
+            # Crafting EC2 html table
+            if len(running_ec2) > 0:
+                ec2_table = """
+                    <h3>Running EC2 Instances: </h3>
+                    <table cellpadding="4" cellspacing="4">
+                    <tr><td><strong>Name</strong></td><td><strong>Instance ID</strong></td><td><strong>Intsance Type</strong></td><td><strong>Key Name</strong></td><td><strong>Region</strong></td><td><strong>Launch Time</strong></td></tr>
+                    """ + \
+                        "\n".join([f"<tr><td>{r['instance_name']}</td><td>{r['id']}</td><td>{r['instance_type']}</td><td>{r['key_pair']}</td><td>{r['region']}</td><td>{r['launch_time']}</td></tr>" for r in running]) \
+                        + """
+                    </table>
+                    <p>Total number of running EC2 instance(s): """ + str(len(running_ec2)) + """
+                    <br />Total number of hidden EC2 instance(s): """ + str(hidden_count) + """</p>"""
+            else:
+                ec2_table = """"""
+            
+            # Crafting RDS html table
+            if len(running_rds) > 0:
+                rds_table = """
+                    <h3>Running RDS Instances: </h3>
+                    <table cellpadding="4" cellspacing="4">
+                    <tr><td><strong>Name</strong></td><td><strong>Engine</strong></td><td><strong>DB Type</strong></td><td><strong>Volume (GB)</strong></td><td><strong>Region</strong></td><td><strong>Launch Time</strong></td></tr>
+                    """ + \
+                        "\n".join([f"<tr><td>{r['db_instance_name']}</td><td>{r['db_engine']}</td><td>{r['db_type']}</td><td>{r['db_storage']}</td><td>{r['region']}</td><td>{r['launch_time']}</td></tr>" for r in running_rds]) \
+                        + """
+                    </table>
+                    <p>Total number of running RDS instance(s): """ + str(len(running_rds)) + """"""
+            else:
+                rds_table = """"""
+            
+            footer = """
+                    <p><a href="https://github.com/z0ph/instance-watcher">Instance Watcher 🖤</a></p>
+                </body>
             </html>
             """
+            # Concatenate html email
+            body_html = header + ec2_table + rds_table + footer
+
             response = ses.send_email(
                 Destination={
                     'ToAddresses': recipients,
